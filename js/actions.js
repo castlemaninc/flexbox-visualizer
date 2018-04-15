b538a9125e48638cf8a46f0517d3e487556d5877
Bryan Castleman <castlemaninc@gmail.com>
2018-04-14 19:20:25 -0700

    adding the on method for justify-content

--- js/actions.js
+++ js/actions.js
@@ -25,16 +25,48 @@ $('.flex-direction > .control-panel > .btn.column-reverse').on('click', function
 // flex-wrap 
 
 $('.flex-wrap > .control-panel > .btn.nowrap').on('click', function(){
-	console.log('clicked');
+	// console.log('clicked');
 	$('.container-2').css('flex-wrap', 'nowrap');
 });
 
 $('.flex-wrap > .control-panel > .btn.wrap').on('click', function(){
-	console.log('clicked');
+	// console.log('clicked');
 	$('.container-2').css('flex-wrap', 'wrap');
 });
 
 $('.flex-wrap > .control-panel > .btn.wrap-reverse').on('click', function(){
-	console.log('clicked');
+	// console.log('clicked');
 	$('.container-2').css('flex-wrap', 'wrap-reverse');
+});
+
+// justify-content
+
+$('.justify-content > .control-panel > .btn.flex-start').on('click', function(){
+	console.log('clicked');
+	$('.container-3').css('justify-content', 'flex-start');
+});
+
+$('.justify-content > .control-panel > .btn.flex-end').on('click', function(){
+	console.log('clicked');
+	$('.container-3').css('justify-content', 'flex-end');
+});
+
+$('.justify-content > .control-panel > .btn.center').on('click', function(){
+	console.log('clicked');
+	$('.container-3').css('justify-content', 'center');
+});
+
+$('.justify-content > .control-panel > .btn.space-between').on('click', function(){
+	console.log('clicked');
+	$('.container-3').css('justify-content', 'space-between');
+});
+
+$('.justify-content > .control-panel > .btn.space-around').on('click', function(){
+	console.log('clicked');
+	$('.container-3').css('justify-content', 'space-around');
+});
+
+$('.justify-content > .control-panel > .btn.space-evenly').on('click', function(){
+	console.log('clicked');
+	$('.container-3').css('justify-content', 'space-evenly');
 });
