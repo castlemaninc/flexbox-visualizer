17d84f8e84f23e728ef8094812db445e5d7c2bfd
Bryan Castleman <castlemaninc@gmail.com>
2018-04-14 19:11:40 -0700

    shrunk width of container-2 and added jquery for flex-wrap properties

--- js/actions.js
+++ js/actions.js
@@ -1,24 +1,40 @@
 console.log("ready");
 
+// flex-direction
+
 $('.flex-direction > .control-panel > .btn.row').on('click', function(){
-	console.log('clicked');
+	// console.log('clicked');
 	$('.container-1').css('flex-direction', 'row');
 });
 
 $('.flex-direction > .control-panel > .btn.row-reverse').on('click', function(){
-	console.log('clicked');
+	// console.log('clicked');
 	$('.container-1').css('flex-direction', 'row-reverse');
 });
 
 $('.flex-direction > .control-panel > .btn.column').on('click', function(){
-	console.log('clicked');
+	// console.log('clicked');
 	$('.container-1').css('flex-direction', 'column');
 });
 
 $('.flex-direction > .control-panel > .btn.column-reverse').on('click', function(){
-	console.log('clicked');
+	// console.log('clicked');
 	$('.container-1').css('flex-direction', 'column-reverse');
 });
 
-	
+// flex-wrap 
 
+$('.flex-wrap > .control-panel > .btn.nowrap').on('click', function(){
+	console.log('clicked');
+	$('.container-2').css('flex-wrap', 'nowrap');
+});
+
+$('.flex-wrap > .control-panel > .btn.wrap').on('click', function(){
+	console.log('clicked');
+	$('.container-2').css('flex-wrap', 'wrap');
+});
+
+$('.flex-wrap > .control-panel > .btn.wrap-reverse').on('click', function(){
+	console.log('clicked');
+	$('.container-2').css('flex-wrap', 'wrap-reverse');
+});
