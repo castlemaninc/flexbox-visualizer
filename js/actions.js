3d47b219723f4b6b511939e24d53e264bb213a27
Bryan Castleman <castlemaninc@gmail.com>
2018-04-14 18:54:08 -0700

    adding jquery to switch flexbox properties on container-1

--- js/actions.js
+++ js/actions.js
@@ -1,1 +1,24 @@
-console.log("hello");
+console.log("ready");
+
+$('.flex-direction > .control-panel > .btn.row').on('click', function(){
+	console.log('clicked');
+	$('.container-1').css('flex-direction', 'row');
+});
+
+$('.flex-direction > .control-panel > .btn.row-reverse').on('click', function(){
+	console.log('clicked');
+	$('.container-1').css('flex-direction', 'row-reverse');
+});
+
+$('.flex-direction > .control-panel > .btn.column').on('click', function(){
+	console.log('clicked');
+	$('.container-1').css('flex-direction', 'column');
+});
+
+$('.flex-direction > .control-panel > .btn.column-reverse').on('click', function(){
+	console.log('clicked');
+	$('.container-1').css('flex-direction', 'column-reverse');
+});
+
+	
+
